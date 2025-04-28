ae195e5560aabdf1c778ca85d07e9fa17c9157d2
김서나 <sgim0803@gnu.ac.kr>
2025-04-28 17:41:21 +0900

    로고 적용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
-import Mypage from './pages/mypage/mypage';
+import MyPage from './pages/mypage/mypage';
 import Login from './screens/Login/Login';  
 import Side from './components/side'; 
 
