18fb04d45dbfeeb293362994487fcdfc6d3c1f94
yoooung03 <129645571+yoooung03@users.noreply.github.com>
2025-05-27 03:29:30 +0900

    Payment.js 수정사항 커밋

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
+import { getEnv } from './getEnv';
+
+const BASE_URL = getEnv('BASE_URL', 'http://localhost:4000');
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
 // 요청 시 Authorization 헤더 자동 추가
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('accessToken');
+    const token ='eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIyOSIsImlhdCI6MTc0ODI4MzU0NSwiZXhwIjoxNzQ4Mjg3MTQ1fQ.ga0AtXdcRzFiNwXgLB4UYineGZxpgj1GWtL1ih58Jt0';
+     // const token = localStorage.getItem('accessToken');
     if (token) {
       config.headers['Authorization'] = `Bearer ${token}`;
     }
