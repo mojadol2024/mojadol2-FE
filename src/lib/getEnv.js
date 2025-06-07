8e2bd5dc6327bfee8259311407e9bcd108f7ded5
김서나 <sgim0803@gmail.com>
2025-06-08 04:37:27 +0900

    getEnv.js

--- src/lib/getEnv.js
+++ src/lib/getEnv.js
@@ -1,6 +1,6 @@
-export const getEnv = (key, fallback = '') => {
-  if (typeof window !== 'undefined' && window._env_ && window._env_[`REACT_APP_${key}`]) {
-    return window._env_[`REACT_APP_${key}`];
+export function getEnv(key, fallback = '') {
+  if (typeof window !== 'undefined' && window._env_ && window._env_[key]) {
+    return window._env_[key];
   }
   return process.env[`REACT_APP_${key}`] || fallback;
-};
+}
