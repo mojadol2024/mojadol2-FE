14ffd36bde01bb4836588547bc4215e93da1ece2
byeongyeongtae <bhj691226@naver.com>
2025-06-11 22:44:33 +0900

    env.js test

--- src/lib/axiosInstance.js
+++ src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getEnv } from './getEnv';
 
-const BASE_URL = getEnv('BASE_URL', 'http://localhost:4000');
+const BASE_URL = getEnv('API_BASE_URL');
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
--- src/lib/getEnv.js
+++ src/lib/getEnv.js
@@ -1,24 +1,6 @@
 export function getEnv(key, fallback = '') {
-  if (typeof window !== 'undefined') {
-    const env = window._env_ || {};
-    
-    // OS 감지
-    const platform = navigator.platform.toLowerCase();
-    const isMac = platform.includes('mac');
-    const isWindows = platform.includes('win');
-
-    // OS별 키 지정
-    const osKey = isMac
-      ? `${key}_MAC`
-      : isWindows
-      ? `${key}_WIN`
-      : key; // 기본값
-
-    // OS에 맞는 키 우선 반환
-    if (env[osKey]) return env[osKey];
-
-    // 없으면 일반 키 시도
-    if (env[key]) return env[key];
+  if (typeof window !== 'undefined' && window._env_ && window._env_[key]) {
+    return window._env_[key];
   }
   return fallback;
 }
