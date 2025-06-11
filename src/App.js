e091161c0a321dc7450b90710469744cd96af15a
leewoolim <woolim318@gmail.com>
2025-06-12 00:45:33 +0900

    어떄?

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import SpellingCorrection from './pages/spellingCorrection/SpellingCorrection';
 import SignUp from './screens/SignUp/SignUp';
 import FindId from './screens/FindId/FindId';
 import MainLayout from './components/layout/MainLayout';
-import Homepage from './screens/Login/Home/homepage';
+import Homepage from './screens/Home/homepage';
 import InterviewMain from './pages/main/InterviewMain';
 import PasswordResetPage from './screens/Login/PasswordResetPage';
 
